c7ab86e71f6e85805b8a2810a392a4f8317af784
Jim Kang <jimkang@gmail.com>
2018-06-17 10:48:20 -0400

    Get archive-specific logs into update-repos.sh.

--- update-repos.sh
+++ update-repos.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
 root=/mnt/storage/archives/repos
+tasksdir=/opt/archiving-tasks
+
+. ~/.keychain/raspberrypi-sh
 
 for dir in $root/*/
+
 do
     dir=${dir%*/}
+    archivename=$(basename $dir)
+    current_log="${tasksdir}/${archivename}-backup.log"
+    echo $current_log
+    date >> ${current_log}
     cd ${root}/${dir##*/}
-    git pull origin master 
+    git pull origin master >> ${current_log} 
+    echo "--END--" >> ${current_log}
 done
 
