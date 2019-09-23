34ebfb4e7ee2c4b7377c64023ef47d60113ce1e8
Jim Kang <jimkang@gmail.com>
2019-09-23 08:48:38 -0400

    Fix linter errors.

--- update-repos.sh
+++ update-repos.sh
@@ -3,18 +3,18 @@
 root=/mnt/storage/archives/repos
 tasksdir=/opt/archiving-tasks
 
-. ~/.keychain/raspberrypi-sh
+~/.keychain/raspberrypi-sh
 
 for dir in $root/*/
 
 do
     dir=${dir%*/}
-    archivename=$(basename $dir)
+    archivename=$(basename "$dir")
     current_log="${tasksdir}/${archivename}-backup.log"
-    echo $current_log
-    date >> ${current_log}
-    cd ${root}/${dir##*/}
-    git pull origin master >> ${current_log} 
-    echo "--END--" >> ${current_log}
+    echo "$current_log"
+    date >> "${current_log}"
+    cd ${root}/"${dir##*/}" || exit
+    git pull origin master >> "${current_log}"
+    echo "--END--" >> "${current_log}"
 done
 
